0ddd5c7151983ee3369b5b7fb9e0b56d3db1bee1
RIPv2 <13338601+ripv2@user.noreply.gitee.com>
2026-05-21 12:16:12 +0800

    配置 ESA 部署：添加外部包支持 postgres 和 bcryptjs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverPort: process.env.PORT || 3000,
+  experimental: {
+    serverComponentsExternalPackages: ['postgres', 'bcryptjs'],
+  },
 }
 
 module.exports = nextConfig
