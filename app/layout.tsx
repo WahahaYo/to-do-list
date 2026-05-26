ee79267817ad9c30d3e7ee14e57278fd6c0f4ecc
RIPv2 <13338601+ripv2@user.noreply.gitee.com>
2026-05-26 10:16:54 +0800

    fix: charset改为charSet修复TypeScript错误

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,7 +58,7 @@ export default function RootLayout({
     <html lang="zh-CN">
       <head>
         {/* 基础meta标签 */}
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
